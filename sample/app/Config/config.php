d4718528e3aec587e44b1609b7917fcf9a174adf
yassirhoussen <yassirhoussen.abdullah@gmail.com>
2015-06-08 17:08:52 +0200

    removed unused file

--- sample/app/Config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config = array(
-		'url' => 'http://192.168.0.34:5984/'
-	);
-return $config;
-
-?>
--- sample/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-	session_start();
-	header("Location: app/Application/index.php");
-	exit(0);
-?>
